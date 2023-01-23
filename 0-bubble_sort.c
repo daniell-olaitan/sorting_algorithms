1c25b155bbbd7b7ab5391bd616b8687be1f4cfe1
daniell-olaitan <daniell.olaitan@gmail.com>
2023-01-23 11:05:39 +0100

    task 0 c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -20,11 +20,11 @@ void bubble_sort(int *array, size_t size)
 			swap = 0;
 			for (j = 0; j < size - 1; ++j)
 			{
-				if (array[j] > array[j+1])
+				if (array[j] > array[j + 1])
 				{
 					tmp = array[j];
-					array[j] = array[j+1];
-					array[j+1] = tmp;
+					array[j] = array[j + 1];
+					array[j + 1] = tmp;
 					swap = 1;
 					print_arr(array, size);
 				}
