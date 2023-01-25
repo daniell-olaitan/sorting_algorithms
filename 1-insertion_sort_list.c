bcecf0473cb93c248b7533e4c11a53e5b32cf23d
daniell-olaitan <daniell.olaitan@gmail.com>
2023-01-25 11:56:53 +0100

    a

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,5 +1,7 @@
 #include "sort.h"
 
+#include "sort.h"
+
 /**
  * insertion_sort_list - sorts a doubly linked list in ascending order
  * using an insertion sort
@@ -8,7 +10,7 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current = NULL, *pick = NULL, *tmp = NULL;
+	listint_t *current = NULL, *pick = NULL;
 
 	if (!list || !(*list) || !(*list)->next)
 		return;
@@ -28,11 +30,10 @@ void insertion_sort_list(listint_t **list)
 			if (pick->next)
 				pick->next->prev = pick->prev;
 
+			pick->prev = pick->prev->prev;
 			pick->prev->next = pick->next;
-			tmp = pick->prev->prev;
 			pick->prev->prev = pick;
 			pick->next = pick->prev;
-			pick->prev = tmp;
 
 			print_list(*list);
 		}
